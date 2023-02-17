460a6195bcf607783082a13417ddd22a774f0e26
Lunarmagpie <bambolambo0@gmail.com>
2023-02-16 21:48:26 -0500

    chagne stuff

--- packages/roc/2023.2.15/build.sh
+++ packages/roc/2023.2.15/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget "https://github.com/roc-lang/roc/releases/download/nightly/roc_nightly-linux_x86_64-2023-02-13-e3f11a7.tar.gz" -O "roc.tar.gz"
+wget "https://github.com/roc-lang/roc/releases/download/nightly/roc_nightly-linux_x86_64-2023-02-15-b054977.tar.gz" -O "roc.tar.gz"
 mkdir roc
 tar -xf "roc.tar.gz" -C "roc"
 
